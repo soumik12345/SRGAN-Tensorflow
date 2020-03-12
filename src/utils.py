66e2bc72cb86b881ab097e5ab40ca98ad7bd2830
soumik12345 <19soumik.rakshit96@gmail.com>
2020-03-12 17:22:08 +0530

    Added Training Code

--- src/utils.py
+++ src/utils.py
@@ -31,6 +31,36 @@ def get_checkpoint_pretrain(model, optimizer, checkpoint_dir='./pretrain_checkpo
     return checkpoint, checkpoint_manager
 
 
+def get_checkpoint_train(models, optimizers, checkpoint_dir='./train_checkpoints/'):
+    '''Get Training Checkpoints
+    Params:
+        models          -> [generator, discriminator]
+        optimizers      -> [gen_optimizer, dis_optimizer]
+        checkpoint_dir  -> Checkpoint Directory
+    '''
+    checkpoint = tf.train.Checkpoint(
+        step=tf.Variable(0, name='step'),
+        gen_model = models[0], dis_model = models[1],
+        gen_optimizer=optimizers[0], dis_optimizer=optimizers[1]
+    )
+    checkpoint_manager = tf.train.CheckpointManager(
+        checkpoint=checkpoint,
+        directory=checkpoint_dir,
+        max_to_keep=3
+    )
+    if checkpoint_manager.latest_checkpoint:
+        checkpoint.restore(checkpoint_manager.latest_checkpoint)
+        print(
+            'Loaded ckpt from {} at step {}.'.format(
+                checkpoint_manager.latest_checkpoint,
+                checkpoint.step.numpy()
+            )
+        )
+    else:
+        print("Training from scratch....")
+    return checkpoint, checkpoint_manager
+
+
 def set_memory_growth():
     gpus = tf.config.experimental.list_physical_devices('GPU')
     if gpus:
--- train.py
+++ train.py
@@ -74,7 +74,7 @@ class Trainer:
         return loss_mse
 
     def pretrain(self):
-        self.checkpoint, self.checkpoint_manager = get_checkpoint_pretrain(
+        self.pre_train_checkpoint, self.pre_train_checkpoint_manager = get_checkpoint_pretrain(
             self.generator, self.g_optimizer,
             checkpoint_dir=self.config['pretrain_checkpoint_dir']
         )
@@ -88,9 +88,78 @@ class Trainer:
                     tf.summary.scalar('pretrain/mse_loss', loss, step=tf.cast(iteration, tf.int64))
                     self.pretrain_summary_writer.flush()
                     iteration += 1
-            self.checkpoint_manager.save()
+            self.pre_train_checkpoint_manager.save()
             print(
                 'Pre-trained Generator saved at {}'.format(
-                    self.checkpoint_manager.latest_checkpoint
+                    self.pre_train_checkpoint_manager.latest_checkpoint
                 )
             )
+
+    @tf.function
+    def train_step(self, x, y):
+        valid = tf.ones((x.shape[0],) + self.disc_patch)
+        fake = tf.zeros((x.shape[0],) + self.disc_patch)
+        with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+            fake_hr = self.generator(x)
+            valid_prediction = self.discriminator(y)
+            fake_prediction = self.discriminator(fake_hr)
+            c_loss = content_loss(y, fake_hr)
+            adv_loss = 1e-3 * tf.keras.losses.BinaryCrossentropy()(valid, fake_prediction)
+            mse_loss = tf.keras.losses.MeanSquaredError()(y, fake_hr)
+            perceptual_loss = c_loss + adv_loss + mse_loss
+            valid_loss = tf.keras.losses.BinaryCrossentropy()(valid, valid_prediction)
+            fake_loss = tf.keras.losses.BinaryCrossentropy()(fake, fake_prediction)
+            d_loss = tf.add(valid_loss, fake_loss)
+        gen_grads = gen_tape.gradient(perceptual_loss, self.generator.trainable_variables)
+        self.g_optimizer.apply_gradients(zip(gen_grads, self.generator.trainable_variables))
+        disc_grads = disc_tape.gradient(d_loss, self.discriminator.trainable_variables)
+        self.d_optimizer.apply_gradients(zip(disc_grads, self.discriminator.trainable_variables))
+        return d_loss, adv_loss, c_loss, mse_loss
+
+    def train(self):
+        self.checkpoint, self.checkpoint_manager = get_checkpoint_train(
+            [self.generator, self.discriminator],
+            [self.g_optimizer, self.d_optimizer],
+            checkpoint_dir=self.config['train_checkpoint_dir']
+        )
+        self.train_summary_writer = tf.summary.create_file_writer(self.config['train_logdir'])
+        with self.train_summary_writer.as_default():
+            iterations = 1
+            for epoch in range(1, self.config['train_epochs'] + 1):
+                print('Epoch: {}'.format(epoch))
+                for x, y in tqdm(self.dataset):
+                    disc_loss, adv_loss, c_loss, mse_loss = self.train_step(x, y)
+                    denorm_x = tf.cast(255 * x, tf.uint8)
+                    denorm_y = tf.cast(255 * (y + 1.0) / 2.0, tf.uint8)
+                    denorm_pred = tf.cast(255 * (self.generator.predict(x) + 1.0) / 2.0, tf.uint8)
+                    psnr_batch = []
+                    for i in range(self.config['batch_size']):
+                        psnr_batch.append(get_psnr(denorm_y[i], denorm_pred[i]))
+                    psnr = sum(psnr_batch) / self.config['batch_size']
+                    tf.summary.scalar('train/adversarial_loss', adv_loss, step=iterations)
+                    tf.summary.scalar('train/content_loss', content_loss, step=iterations)
+                    tf.summary.scalar('train/mse_loss', mse_loss, step=iterations)
+                    tf.summary.scalar('train/discriminator_loss', disc_loss, step=iterations)
+                    tf.summary.scalar(
+                        'learning_rate/learning_rate_G',
+                        self.g_optimizer.lr(iterations),
+                        step=iterations
+                    )
+                    tf.summary.scalar(
+                        'learning_rate/learning_rate_D',
+                        self.d_optimizer.lr(iterations),
+                        step=iterations
+                    )
+                    tf.summary.scalar('train/psnr', psnr, step=iterations)
+                    tf.summary.image('Low Res', denorm_x, step=iterations)
+                    tf.summary.image('High Res', denorm_y, step=iterations)
+                    tf.summary.image('Generated', denorm_pred, step=iterations)
+                    self.train_summary_writer.flush()
+                    iterations += 1
+                if epoch % self.config['save_interval'] == 0:
+                    self.checkpoint_manager.save()
+                    print(
+                        'Model Checkpoints saved at {}'.format(
+                            self.checkpoint_manager.latest_checkpoint
+                        )
+                    )
